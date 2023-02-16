89ad8463ab707104be684f8c954a13ed60c61a39
rkskekzzz <wkdlfflxh@naver.com>
2023-02-16 18:35:30 +0900

    ✨ pages init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
     return (
