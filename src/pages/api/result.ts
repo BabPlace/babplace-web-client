bed9fc310b3f812ebd97168acbcab04ae23244ba
rkskekzzz <wkdlfflxh@naver.com>
2023-04-01 00:42:58 +0900

    🐛 버그 수정

--- src/pages/api/result.ts
+++ src/pages/api/result.ts
@@ -3,52 +3,52 @@ import type { Restaurant, ResultResponse } from '../../interfaces';
 
 const dummyResultResponse: ResultResponse = [
   {
-    name: 'bugger king',
+    restaurantName: 'bugger king',
     good: ['닉네임은 요기', 'maybe english', 'Capital?', '1010101'],
     bad: [],
   },
   {
-    name: 'pizza queen',
+    restaurantName: 'pizza queen',
     good: ['최진용'],
     bad: ['su', 'choi', '한찬호'],
   },
   {
-    name: '라면킹',
+    restaurantName: '라면킹',
     good: ['최진용', '한찬호'],
     bad: ['su', 'choi'],
   },
   {
-    name: 'pizza queen',
+    restaurantName: 'pizza queen',
     good: ['최진용'],
     bad: ['su', 'choi', '한찬호'],
   },
   {
-    name: '라면킹',
+    restaurantName: '라면킹',
     good: ['최진용', '한찬호'],
     bad: ['su', 'choi'],
   },
   {
-    name: 'pizza queen',
+    restaurantName: 'pizza queen',
     good: ['최진용'],
     bad: ['su', 'choi', '한찬호'],
   },
   {
-    name: '라면킹',
+    restaurantName: '라면킹',
     good: ['최진용', '한찬호'],
     bad: ['su', 'choi'],
   },
   {
-    name: '라면나라 피자왕자',
+    restaurantName: '라면나라 피자왕자',
     good: [],
     bad: ['su', 'choi'],
   },
   {
-    name: '마라마라 마라탕',
+    restaurantName: '마라마라 마라탕',
     good: [],
     bad: ['su', 'choi'],
   },
   {
-    name: '김밥지옥',
+    restaurantName: '김밥지옥',
     good: [],
     bad: ['su', 'choi'],
   },
