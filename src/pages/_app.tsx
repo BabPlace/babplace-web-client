0cbf69734e21cb0bfbfbd907fd31b4f0d452451c
rkskekzzz <wkdlfflxh@naver.com>
2023-04-16 02:28:07 +0900

    ✨ manifest, icon added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link rel='manifest' href='/manifest.json' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
       </Head>
       <ColorModeContext.Provider value={colorMode}>
