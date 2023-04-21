0be0ac8c805215190a3741c50292a2a05caeca90
rkskekzzz <wkdlfflxh@naver.com>
2023-04-22 00:03:19 +0900

    🔥 remove viewport tag in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
           <meta name='theme-color' content='#47b8e0'></meta>
           <link rel='manifest' href='/manifest.json' />
           {/* favicon */}
