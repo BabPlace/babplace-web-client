8abdf8bd0bfc71629dbde6b3bd31f6c3d47d5a80
rkskekzzz <wkdlfflxh@naver.com>
2023-04-27 23:10:16 +0900

    🐛 golo card bug fixed

--- src/components/pages/gola/Gola.tsx
+++ src/components/pages/gola/Gola.tsx
@@ -25,79 +25,80 @@ const Gola = ({ isValidUser, restaurants = [] }: Props) => {
     <BaseUI title={title} description={description}>
       <Header showButtons />
       <div className={styles.container}>
-        <Visible visible={isLoading}>
+        {isLoading ? (
           <Loading />
-        </Visible>
-        <Visible visible={!isLoading} style={{ height: '100%' }}>
-          <div className={styles.relative}>
-            {restaurants.map(
-              ({ id, latitude: lat, longitude: lng, name, category, address, distance, restaurantPlaceUrl }, index) =>
-                !loading &&
-                canRender(index) && (
-                  <div key={`gola-card-${id}`} style={{ borderRadius: 'var(--border-radius)' }}>
-                    <ForwardRefNoSSRTinderCard
-                      ref={cardRefs[index]}
-                      className='swipe'
-                      onCardLeftScreen={afterSwipe}
-                      onSwipe={(direction: Direction) => {
-                        addResult({ restaurantId: id, satisfaction: directionToSatisfaction(direction) });
-                      }}
-                      // onSwipeRequirementFulfilled={(direction: Direction) => {
-                      //   console.log(direction);
-                      //   setCurrentDirection(direction);
-                      // }}
-                    >
-                      <div
-                        className={styles.card + ' card'}
-                        // grade={0}
-                        // satisfaction={directionToSatisfaction(currentDirection || '').toLowerCase()}
-                        onMouseDown={(e) => {
-                          console.log(e);
+        ) : (
+          <>
+            <div className={styles.relative}>
+              {restaurants.map(
+                ({ id, latitude: lat, longitude: lng, name, category, address, distance, restaurantPlaceUrl }, index) =>
+                  !loading &&
+                  canRender(index) && (
+                    <div key={`gola-card-${id}`} style={{ borderRadius: 'var(--border-radius)' }}>
+                      <ForwardRefNoSSRTinderCard
+                        ref={cardRefs[index]}
+                        className='swipe'
+                        onCardLeftScreen={afterSwipe}
+                        onSwipe={(direction: Direction) => {
+                          addResult({ restaurantId: id, satisfaction: directionToSatisfaction(direction) });
                         }}
+                        // onSwipeRequirementFulfilled={(direction: Direction) => {
+                        //   console.log(direction);
+                        //   setCurrentDirection(direction);
+                        // }}
                       >
-                        <StaticMap marker={{ position: { lat, lng } }} center={{ lat, lng }} style={mapStyle} level={4} />
-                        <div className={styles.info}>
-                          <FlexRow alignItems='center' justifyContent='space-between'>
-                            <TypoNotoSans text={name} variant='h6' />
-                            <IconButton
-                              onTouchStartCapture={(event) => {
-                                event.stopPropagation();
-                              }}
-                              onClick={() => {
-                                window.open(restaurantPlaceUrl, '_blank');
-                              }}
-                              sx={{ padding: '0px' }}
-                            >
-                              <InfoIcon />
-                            </IconButton>
-                          </FlexRow>
-                          <TypoNotoSans text={categoryFormat(category)} />
-                          <TypoNotoSans variant='caption'>{distance === -1 ? address : distanceFormat(distance)}</TypoNotoSans>
+                        <div
+                          className={styles.card + ' card'}
+                          // grade={0}
+                          // satisfaction={directionToSatisfaction(currentDirection || '').toLowerCase()}
+                          onMouseDown={(e) => {
+                            console.log(e);
+                          }}
+                        >
+                          <StaticMap marker={{ position: { lat, lng } }} center={{ lat, lng }} style={mapStyle} level={4} />
+                          <div className={styles.info}>
+                            <FlexRow alignItems='center' justifyContent='space-between'>
+                              <TypoNotoSans text={name} variant='h6' />
+                              <IconButton
+                                onTouchStartCapture={(event) => {
+                                  event.stopPropagation();
+                                }}
+                                onClick={() => {
+                                  window.open(restaurantPlaceUrl, '_blank');
+                                }}
+                                sx={{ padding: '0px' }}
+                              >
+                                <InfoIcon />
+                              </IconButton>
+                            </FlexRow>
+                            <TypoNotoSans text={categoryFormat(category)} />
+                            <TypoNotoSans variant='caption'>{distance === -1 ? address : distanceFormat(distance)}</TypoNotoSans>
+                          </div>
                         </div>
-                      </div>
-                    </ForwardRefNoSSRTinderCard>
-                  </div>
-                )
-            )}
-          </div>
-          <div className={styles.card_action_buttons}>
-            <IconButton onClick={goBack} className={styles.undo}>
-              <ReplayIcon />
-            </IconButton>
-            <IconButton onClick={swipeDown} className={styles.verybad}>
-              <SickIcon />
-            </IconButton>
-            <IconButton onClick={swipeLeft} className={styles.bad}>
-              <VeryDissatisfiedIcon />
-            </IconButton>
-            <IconButton onClick={swipeRight} className={styles.good}>
-              <SatisfiedAltIcon />
-            </IconButton>
-            <IconButton onClick={swipeUp} className={styles.verygood}>
-              <VerySatisfiedIcon />
-            </IconButton>
-          </div>
-        </Visible>
+                      </ForwardRefNoSSRTinderCard>
+                    </div>
+                  )
+              )}
+            </div>
+            <div className={styles.card_action_buttons}>
+              <IconButton onClick={goBack} className={styles.undo}>
+                <ReplayIcon />
+              </IconButton>
+              <IconButton onClick={swipeDown} className={styles.verybad}>
+                <SickIcon />
+              </IconButton>
+              <IconButton onClick={swipeLeft} className={styles.bad}>
+                <VeryDissatisfiedIcon />
+              </IconButton>
+              <IconButton onClick={swipeRight} className={styles.good}>
+                <SatisfiedAltIcon />
+              </IconButton>
+              <IconButton onClick={swipeUp} className={styles.verygood}>
+                <VerySatisfiedIcon />
+              </IconButton>
+            </div>
+          </>
+        )}
       </div>
     </BaseUI>
   );
@@ -113,15 +114,3 @@ const ForwardRefNoSSRTinderCard = React.forwardRef<API, any>((props, ref) => <No
 const title = '식당 만족도 조사 | 골라밥 🍚';
 const description = '원하는 식당, 원하지 않는 식당을 표현하세요!';
 const mapStyle = { width: '99.5%', height: '99.5%', borderRadius: 'var(--border-radius)' };
-
-const StyledCard = styled.div<{ satisfaction: string; grade: number }>`
-  &&::after {
-    content: '';
-    width: 100%;
-    background: ${({ satisfaction }) => `rgb(var(--gola-${satisfaction}-rgb))`};
-    height: 100%;
-    position: absolute;
-    top: 0;
-    z-index: 99999;
-  }
-`;
