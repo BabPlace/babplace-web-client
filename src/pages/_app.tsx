c89d6d89bd082860285e4bcfbc97af74b507889e
chanho <chhqm2759@naver.com>
2023-04-28 21:53:21 +0900

    ✨ add ga code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,8 @@ import createEmotionCache from '@/utils/createEmotionCache';
 import { ColorModeContext, SelectsContext } from '@/components';
 import { useTheme, useSelects, useServiceWorker } from '@/hooks';
 import '@/styles/globals.css';
+import Script from 'next/script'
+import * as gtag from '../lib/gtag'
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -22,6 +24,25 @@ export default function App({ Component, emotionCache = clientSideEmotionCache, 
         <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover' />
         <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
       </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <CacheProvider value={emotionCache}>
         <ColorModeContext.Provider value={colorMode}>
           <SelectsContext.Provider value={selects}>
