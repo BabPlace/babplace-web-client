6fee2c8e0f816d58876ac267b1dca04181c136f5
rkskekzzz <wkdlfflxh@naver.com>
2023-05-02 23:43:07 +0900

    ♻️ isHidden to css module

--- src/layouts/container/SwipeableEdgeDrawer.tsx
+++ src/layouts/container/SwipeableEdgeDrawer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDrawer, useQuery } from '@/hooks';
+import cn from 'classnames';
 import Visible from './Visible';
 import styles from '@/styles/SwipeableEdgeDrawer.module.css';
 import styled from '@emotion/styled';
@@ -16,7 +17,14 @@ const SwipeableEdgeDrawer = ({ children, height, isHidden, swipeUp = true, showP
   const { drawerRef, open } = useDrawer(swipeUp);
   const { isDefault } = useQuery();
   return (
-    <StyledDrawer className={styles.container} ref={drawerRef} isOpen={open} isHidden={isHidden} height={height} isDefault={isDefault}>
+    <StyledDrawer
+      className={cn(styles.container, isHidden ? 'hidden' : '')}
+      ref={drawerRef}
+      isOpen={open}
+      isHidden={isHidden}
+      height={height}
+      isDefault={isDefault}
+    >
       <Visible visible={showPuller} className={styles.puller} />
       {children}
     </StyledDrawer>
@@ -33,7 +41,6 @@ const StyledDrawer = styled.div<{ isOpen: boolean; isHidden?: boolean; height?: 
         : isDefault
         ? 'var(--drawer-default-height)'
         : 'calc(var(--drawer-default-height) - var(--drawer-list-height) * 2)'};
-    bottom: ${({ isHidden }) => (isHidden ? '-100%' : '0')};
     height: ${({ height }) => height};
     z-index: ${({ isOpen }) => (isOpen ? '2' : '0')};
   }
