c02fb3d9f69b874c44f46f5c87759ccc7de14707
rkskekzzz <wkdlfflxh@naver.com>
2023-05-10 00:01:21 +0900

    ✨ sw update

--- public/sw_with_push_manager.js
+++ public/sw_with_push_manager.js
@@ -36,15 +36,16 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('push', (event) => {
   let data = event.data.json();
-  const image = 'https://cdn.glitch.com/614286c9-b4fc-4303-a6a9-a4cef0601b74%2Flogo.png?v=1605150951230';
+  const image = 'https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1566557331/noticon/d5hqar2idkoefh6fjtpu.png';
   const options = {
-    body: data.options.body,
+    body: data.teamId,
     icon: image,
   };
-  self.registration.showNotification(data.title, options);
+  self.registration.showNotification(data.teamName, options);
 });
 
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
+  console.log(event.notification.data.json());
   event.waitUntil(self.clients.openWindow('https://web.dev'));
 });
