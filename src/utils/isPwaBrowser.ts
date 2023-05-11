e31d2385e8c0774b3596d7f43c699641ea40fa30
rkskekzzz <wkdlfflxh@naver.com>
2023-05-11 11:07:20 +0900

    🐛 add try catch

--- src/utils/isPwaBrowser.ts
+++ src/utils/isPwaBrowser.ts
@@ -1,12 +1,15 @@
 export default function isPwaBrowser() {
   if (typeof window === undefined) return false;
-  if (
-    window.matchMedia('(display-mode: standalone)').matches ||
-    // @ts-ignore
-    window.navigator.standalone ||
-    document.referrer.includes('android-app://')
-  ) {
-    return true;
+  try {
+    if (
+      window.matchMedia('(display-mode: standalone)').matches ||
+      // @ts-ignore
+      window.navigator.standalone ||
+      document.referrer.includes('android-app://')
+    ) {
+      return true;
+    }
+  } catch (e) {
+    return false;
   }
-  return false;
 }
