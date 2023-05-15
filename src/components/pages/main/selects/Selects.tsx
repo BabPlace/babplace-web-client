69961a9160d9298e0a880de67d7cc1f9cbe00e1c
rkskekzzz <wkdlfflxh@naver.com>
2023-05-16 01:13:53 +0900

    ✨ like guide added

--- src/components/pages/main/selects/Selects.tsx
+++ src/components/pages/main/selects/Selects.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useConfirm, useInput, useLike } from '@/hooks';
 import { FullScreenModal, Confirm } from '@/components';
 import { RemoveIcon, AddIcon } from '@/icons';
@@ -9,6 +9,8 @@ import SelectsBox from './SelectsBox';
 import SearchBox from '../search/SearchBox';
 import type { SelectPlace } from '@/interfaces';
 import styles from '@/styles/Selects.module.css';
+import styled from '@emotion/styled';
+import { TypoNotoSans } from '@/layouts';
 
 const Selects = () => {
   const { isShow: isConfirmShow, open, handleClose } = useConfirm();
@@ -66,15 +68,69 @@ const SelectsItems = ({ value, open, set }: { value: string; open: () => void; s
 const LikeItems = ({ value }: { value: string }) => {
   const { addSelects, hide } = useContext(SelectsContext);
   const { likeItems } = useLike(null);
+  const [continuity, setContinuity] = useState<string[]>([]);
+  const [isShowToastMessage, setIsShowToastMessage] = useState(false);
+
+  useEffect(() => {
+    if (continuity.length === 0) return;
+    let timer: NodeJS.Timeout;
+    if (isShowToastMessage) {
+      timer = setTimeout(() => {
+        setIsShowToastMessage(false);
+      }, 3000);
+    } else {
+      setContinuity([]);
+    }
+    return () => {
+      console.log('clear : ', timer);
+      clearTimeout(timer);
+    };
+  }, [isShowToastMessage, continuity]);
 
   return (
-    <SelectsBox
-      selects={filterPlace(likeItems, value)}
-      actionButton={<AddIcon fontSize='small' color='info' />}
-      action={(select) => {
-        addSelects(select);
-      }}
-      emptyAction={hide}
-    />
+    <>
+      <SelectsBox
+        selects={filterPlace(likeItems, value)}
+        actionButton={<AddIcon fontSize='small' color='info' />}
+        action={(select) => {
+          setIsShowToastMessage(true);
+          addSelects(select);
+          if (!continuity.includes(select.id)) setContinuity((prev) => [...prev, select.id]);
+        }}
+        emptyAction={hide}
+      />
+      <ToastMessage isShowToastMessage={isShowToastMessage}>
+        <TypoNotoSans>{continuity.length}개의 식당을 추가하였습니다</TypoNotoSans>
+      </ToastMessage>
+    </>
   );
 };
+
+const ToastMessage = styled.div<{ isShowToastMessage: boolean }>`
+  display: ${({ isShowToastMessage }) => (isShowToastMessage ? '' : 'none')};
+  position: absolute;
+  border-radius: 100px;
+  border: 1px solid rgba(var(--secondary-foreground-rgba));
+  color: rgba(var(--secondary-foreground-rgba));
+  padding: 5px 10px;
+  bottom: 100px;
+  left: 50%;
+
+  transform: translateX(-50%);
+  animation: fadeIn 3s ease-in-out 0s forwards;
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    30% {
+      opacity: 1;
+    }
+    60% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`;
