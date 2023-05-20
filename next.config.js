7c9ef3fa435d0afe061b318d7774cb36258bda57
rkskekzzz <wkdlfflxh@naver.com>
2023-05-20 17:12:16 +0900

    ♻️ with docker

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: false,
   swcMinify: true,
   compiler: {
